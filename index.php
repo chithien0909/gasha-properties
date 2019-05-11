1263a2e55fda257a310b4b636ea0039b7b2b3cbf
chithien0909 <chithien0909@gmail.com>
2019-05-12 00:40:58 +0700

    Header and main content

--- index.php
+++ index.php
@@ -4,26 +4,72 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
+
+    <link rel="stylesheet" href="./scss/main.css">
+
     <title>Gasha Properties</title>
 </head>
 <body>
     <div class="site-header">
+        <div class="img-bg">
+            <img src="./images/bg.png" alt="Image Background">
+        </div>
         <div class="header">
             <div class="top-menu">
                 <div class="site-left">
-                    <span class='phone'>Hotline: +84 983910453</span>
-                    <span class='mail'>Email:  info@gashaproperties.com</span>
+                    <span class='phone'>
+                        <img src="./images/icon-phone.svg" alt=Phone"">
+                        +84 983910453
+                    </span>
+                    <span class='mail'>
+                        <img src="./images/icon-mail.svg" alt="Mail">
+                        info@gashaproperties.com
+                    </span>
                 </div>
                 <div class='site-right'>
                     <div class='currencies'>
+                        <img style='width: 25px;height:20px' src="./images/flag-united-kingdom.svg" alt="USD">
+                        <select name="" id="">
+                            <option value="USD">
+                                <span>GBP</span>
+                                <span>30.406đ</span>
+                            </option>
+                        </select>
                     </div>
                     <div class='weather'>
+                        <img style="width: 16px;height: 16px" src="./images/weather-sun.svg" alt="Weather">
+                        <select name="" id="">
+                            <option value="HCM">Ho Chi Minh City 31ºC</option>
+                        </select>
                     </div>
                 </div>
             </div>
-
-            <div class='menu'>
+            <div class='f-menu'>
+                <div class="logo">
+                    <img src="./images/logo.svg" alt="Logo">
+                </div>
+                <div class="menu">
+                    <div class="menu-content">
+                        <ul>
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">For owners</a></li>
+                            <li><a href="#">For tenants</a></li>
+                            <li><a href="#">Contact us</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
+        </div>
+        <div class="main-content">
+            <h1 class="title">
+                Modern Apartment in
+                <br>
+                a new residential complex
+            </h1>
+            <h4 class="sub-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
         </div>
     </div>
 </body>

c2eb11ac55ee030f51bf1090e65c95748b00b193
chithien0909 <chithien0909@gmail.com>
2019-05-11 22:20:11 +0700

    Init web

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Gasha Properties</title>
+</head>
+<body>
+    <div class="site-header">
+        <div class="header">
+            <div class="top-menu">
+                <div class="site-left">
+                    <span class='phone'>Hotline: +84 983910453</span>
+                    <span class='mail'>Email:  info@gashaproperties.com</span>
+                </div>
+                <div class='site-right'>
+                    <div class='currencies'>
+                    </div>
+                    <div class='weather'>
+                    </div>
+                </div>
+            </div>
+
+            <div class='menu'>
+            </div>
+        </div>
+    </div>
+</body>
+</html>

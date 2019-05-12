d6c4a8951f336be37f284765f6c20033b7352281
chithien0909 <chithien0909@gmail.com>
2019-05-12 22:37:05 +0700

    Complete layout and responsive

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function() {
-    const arr= ['usd.png','euro.png','canada.png','hongkong.png','russia',];
+    scrollMenu();
+    const arr= ['usd.png','euro.png','canada.png','hongkong.png','russia.png',];
     $('.currencies select').each(function(){
         let $this = $(this);
         let numberOption = $(this).children('option').length;
@@ -7,17 +8,55 @@ jQuery(document).ready(function() {
         $this.wrap('<div class="select"></div>');
         $this.after('<div class="select-styled"></div>');
         let styleSelectStyle = $this.next('div.select-styled');
-        styleSelectStyle.html(`<img src="./images/${arr[0]}">`+$this.children('option').eq(0).html());
-        var $list = $('<ul />', {
+        styleSelectStyle.html(`<img src="./images/${arr[0]}">`+"<span class='text'>"+$this.children('option').eq(0).text().trim()+"</span>");
+        var list = $('<ul/>', {
             'class': 'select-options'
         }).insertAfter(styleSelectStyle);
 
         for (var i = 0; i < numberOption; i++) {
             $('<li />', {
-                html:`<img src="./images/${arr[i]}">`+ $this.children('option').eq(i).text(),
-                rel: $this.children('option').eq(i).val()
-            }).appendTo($list);
+                html:`<img src="./images/${arr[i]}">`+"<span class='text'>"+$this.children('option').eq(0).text().trim()+"</span>",
+                rel: $this.children('option').eq(i).val(),
+                p: i
+            }).appendTo(list);
         }
+        let listItems = list.children('li');
+        styleSelectStyle.click(function(e){
+           e.stopPropagation();
+           $('div.select-styled.active').not(this).each(function(){
+              $(this).removeClass('active').next('ul.select-options').hide();
+           });
+           $(this).toggleClass('active').next('ul.select-options').toggle();
+        });
+        listItems.click(function(e){
+            e.stopPropagation();
+            const i = parseInt($(this).attr('p'));
+            styleSelectStyle.html(`<img src="./images/${arr[i]}">`+"<span class='text'>"+$(this).text()+"</span>").removeClass('active');
+            $this.val($(this).attr('rel'));
+            list.hide();
+        });
 
+
+        $(document).click(function () {
+            styleSelectStyle.removeClass('active');
+            list.hide();
+        });
+    });
+    $(window).scroll(()=>{
+        scrollMenu();
+        }
+    );
+    function scrollMenu(){
+        let topDistance = $('#responsive-menu').offset().top;
+        if(topDistance>50){
+            if($('#responsive-menu.active').length>0) return;
+            $('#responsive-menu').addClass('active');
+        }else{
+            if($('#responsive-menu.active').length===0) return;
+            $('#responsive-menu').removeClass('active');
+        }
+    }
+    $('.menu-icon').click(function(){
+        $('.menu-content').toggleClass('active');
     });
 });
